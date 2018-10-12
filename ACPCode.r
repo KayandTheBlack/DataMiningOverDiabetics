2a54b987fd6cfe26e6c7db5e01b7b0b8eea9648d
Guillem Ferrer Nicolas <guillem.ferrer.nicolas@gmail.com>
2018-10-12 12:55:38 +0200

    Almost complete ACP
    missing markdown

--- ACPCode.r
+++ ACPCode.r
@@ -4,7 +4,7 @@
 # setwd("D:/karina/docencia/areferenciesPPT/DadesPractiques/CREDSCO")
 # dd <- read.table("credscoClean.csv",header=T, sep=";");
 setwd("C:/Users/guill/Documents/GitHub/DataMiningOverDiabetics")
-dd <- read.csv("processed_data.csv", na.strings = c("?"))
+dd <- read.csv("processed_data.csv")
 
 objects()
 attributes(dd)
@@ -48,21 +48,26 @@ totalIner<- sum(inerProj)
 totalIner
 pinerEix<- 100*inerProj/totalIner
 pinerEix
+png(filename="ACP/InertiaBarplot.png", width=1920, height=1080,units="px")
 barplot(pinerEix)
+dev.off()
 
 #Cummulated Inertia in subspaces, from first principal component to the 8th dimension subspace
-
+png(filename="ACP/Inertia2x2.png", width=1920, height=1080,units="px")
 par(mfrow=c(2,2))
 plot(pinerEix,xlab="Principal component", ylab="% of inertia", ylim=c(0,100), type='b')
-plot(cumsum(x.pvar),xlab="Principal component", ylab="Cumulative % of inertia", ylim=c(0,100), type='b')
+plot(cumsum(inerProj/sum(inerProj)*100),xlab="Principal component", ylab="Cumulative % of inertia", ylim=c(0,100), type='b')
 screeplot(pc1)
 screeplot(pc1,type="l")
+dev.off()
+png(filename="ACP/Inertia1x1.png", width=1920, height=1080,units="px")
 par(mfrow=c(1,1))
 
 tmp <- 100*cumsum(pc1$sdev[1:dim(dcon)[2]]^2)/dim(dcon)[2]
 names(tmp) <- c(1,2,3,4,5,6,7,8)
 barplot(tmp)
 plot(c(1:8),tmp,type='o',xlab="Principal component", ylab="Cumulative % of inertia")
+dev.off()
 percInerAccum<-tmp
 percInerAccum
 
@@ -108,195 +113,112 @@ ze = rep(0,length(etiq)) # WE WILL NEED THIS VECTOR AFTERWARDS FOR THE GRAPHICS
 eje1<-1
 eje2<-2
 eje3<-3
+png(filename="ACP/Individual1.png", width=1920, height=1080,units="px")
 plot(Psi[,eje1],Psi[,eje2])
 text(Psi[,eje1],Psi[,eje2],labels=iden, cex=0.5)
 axis(side=1, pos= 0, labels = F, col="cyan")
 axis(side=3, pos= 0, labels = F, col="cyan")
 axis(side=2, pos= 0, labels = F, col="cyan")
 axis(side=4, pos= 0, labels = F, col="cyan")
+dev.off()
+png(filename="ACP/Individual2.png", width=1920, height=1080,units="px")
 plot(Psi[,eje1],Psi[,eje3])
 text(Psi[,eje1],Psi[,eje3],labels=iden, cex=0.5)
 axis(side=1, pos= 0, labels = F, col="cyan")
 axis(side=3, pos= 0, labels = F, col="cyan")
 axis(side=2, pos= 0, labels = F, col="cyan")
 axis(side=4, pos= 0, labels = F, col="cyan")
+dev.off()
+png(filename="ACP/Individual3.png", width=1920, height=1080,units="px")
 plot(Psi[,eje2],Psi[,eje3])
 text(Psi[,eje2],Psi[,eje3],labels=iden, cex=0.5)
 axis(side=1, pos= 0, labels = F, col="cyan")
 axis(side=3, pos= 0, labels = F, col="cyan")
 axis(side=2, pos= 0, labels = F, col="cyan")
 axis(side=4, pos= 0, labels = F, col="cyan")
-
-#library(rgl)
-#plot3d(Psi[,1],Psi[,2],Psi[,3])
+dev.off()
 
 #Projection of variables
 
 Phi = cor(dcon,Psi)
 
 
-
-
-
-
-
-
-
-#select your axis
-
-X<-Phi[,eje1]
-Y<-Phi[,eje2]
-
-# plot(Psi[,eje1],Psi[,eje2],type="n")
-# axis(side=1, pos= 0, labels = F)
-# axis(side=3, pos= 0, labels = F)
-# axis(side=2, pos= 0, labels = F)
-# axis(side=4, pos= 0, labels = F)
-# arrows(ze, ze, X, Y, length = 0.07,col="blue")
-# text(X,Y,labels=etiq,col="darkblue", cex=0.7)
-
-
-#zooms
-plot(Psi[,eje1],Psi[,eje2],type="n",xlim=c(min(X,0),max(X,0)))
-axis(side=1, pos= 0, labels = F)
-axis(side=3, pos= 0, labels = F)
-axis(side=2, pos= 0, labels = F)
-axis(side=4, pos= 0, labels = F)
-arrows(ze, ze, X, Y, length = 0.07,col="blue")
-text(X,Y,labels=etiq,col="darkblue", cex=0.7)
-
-
-
-#Now we project both cdgs of levels of a selected qualitative variable without
-#representing the individual anymore
-
-plot(Psi[,eje1],Psi[,eje2],type="n",xlim=c(-0.4,0.4))
-axis(side=1, pos= 0, labels = F, col="cyan")
-axis(side=3, pos= 0, labels = F, col="cyan")
-axis(side=2, pos= 0, labels = F, col="cyan")
-axis(side=4, pos= 0, labels = F, col="cyan")
-
-#select your qualitative variable
-k<-4 #dictamen in credsco
-varcat<-dd[,"weight"]
-fdic1 = tapply(Psi[,eje1],varcat,mean)
-fdic2 = tapply(Psi[,eje2],varcat,mean) 
-
-#points(fdic1,fdic2,pch=16,col="blue", labels=levels(varcat))
-text(fdic1,fdic2,labels=levels(varcat),col="blue", cex=0.7)
-
-
-#all qualitative together
-plot(Psi[,eje1],Psi[,eje2],type="n") #,xlim=c(-1,1), ylim=c(-3,1)
-axis(side=1, pos= 0, labels = F, col="cyan")
-axis(side=3, pos= 0, labels = F, col="cyan")
-axis(side=2, pos= 0, labels = F, col="cyan")
-axis(side=4, pos= 0, labels = F, col="cyan")
-
-#nominal qualitative variables
-
-dcat<-c(4,5,6,7,12,13,20,21,22,24,25,26,27,28,29,30)
-#divide categoricals in several graphs if joint representation saturates
-
-#build a palette with as much colors as qualitative variables 
-
-# colors<-c("blue","red","green","orange","darkgreen")
-#alternative
- colors<-rainbow(length(dcat))
-
-c<-1
-for(k in dcat){
-  seguentColor<-colors[c]
-fdic1 = tapply(Psi[,eje1],dd[,k],mean)
-fdic2 = tapply(Psi[,eje2],dd[,k],mean) 
-
-text(fdic1,fdic2,labels=levels(dd[,k]),col=seguentColor, cex=0.6)
-c<-c+1
-}
-legend("bottomleft",names(dd)[dcat],pch=1,col=colors, cex=0.6)
-
-#determine zoom level
-#use the scale factor or not depending on the position of centroids
-# ES UN FACTOR D'ESCALA PER DIBUIXAR LES FLETXES MES VISIBLES EN EL GRAFIC
-#fm = round(max(abs(Psi[,1]))) 
-fm=20
-
-#scale the projected variables
-X<-fm*U[,eje1]
-Y<-fm*U[,eje2]
-
-#represent numerical variables in background
-plot(Psi[,eje1],Psi[,eje2],type="n",xlim=c(-1,1), ylim=c(-3,1))
-#plot(X,Y,type="none",xlim=c(min(X,0),max(X,0)))
-axis(side=1, pos= 0, labels = F, col="cyan")
-axis(side=3, pos= 0, labels = F, col="cyan")
-axis(side=2, pos= 0, labels = F, col="cyan")
-axis(side=4, pos= 0, labels = F, col="cyan")
-
-#add projections of numerical variables in background
-arrows(ze, ze, X, Y, length = 0.07,col="lightgray")
-text(X,Y,labels=etiq,col="gray", cex=0.7)
-
-#add centroids
-c<-1
-for(k in dcat){
-  seguentColor<-colors[c]
+plot.with.zooms <- function(minx,maxx,miny,maxy, axis1, axis2){
+  X<-Phi[,axis1]
+  Y<-Phi[,axis2]
+  #all qualitative together with zooms
+  plot(Psi[,axis1],Psi[,axis2],type="n",xlim=c(minx,maxx), ylim=c(miny,maxy))
+  axis(side=1, pos= 0, labels = F, col="cyan")
+  axis(side=3, pos= 0, labels = F, col="cyan")
+  axis(side=2, pos= 0, labels = F, col="cyan")
+  axis(side=4, pos= 0, labels = F, col="cyan")
   
-  fdic1 = tapply(Psi[,eje1],dd[,k],mean)
-  fdic2 = tapply(Psi[,eje2],dd[,k],mean) 
+  arrows(ze, ze, X, Y, length = 0.07,col="blue")
+  text(X,Y,labels=etiq,col="darkblue", cex=0.7)
   
-  #points(fdic1,fdic2,pch=16,col=seguentColor, labels=levels(dd[,k]))
-  text(fdic1,fdic2,labels=levels(dd[,k]),col=seguentColor, cex=0.6)
-  c<-c+1
-}
-legend("bottomleft",names(dd)[dcat],pch=1,col=colors, cex=0.6)
-
-
-#add ordinal qualitative variables. Ensure ordering is the correct
-
-dordi<-c(8)
-
-
-levels(dd[,dordi[1]])
-#reorder modalities: when required
-dd[,dordi[1]] <- factor(dd[,dordi[1]], ordered=TRUE,  levels= c("WorkingTypeUnknown","altres sit","temporal","fixe","autonom"))
-levels(dd[,dordi[1]])
-col <- 1
-c<-1
-for(k in dordi){
-  seguentColor<-colors[col]
-  fdic1 = tapply(Psi[,eje1],dd[,k],mean)
-  fdic2 = tapply(Psi[,eje2],dd[,k],mean) 
+  #nominal qualitative variables
+  dcat<-c(4,5,12,13,20,21,22,24,25,26,27,28,29,30)
+  #divide categoricals in several graphs if joint representation saturates
+  #build a palette with as much colors as qualitative variables 
+  colors<-rainbow(length(dcat))
+  c<-1
+  for(k in dcat){
+    seguentColor<-colors[c]
+    fdic1 = tapply(Psi[,axis1],dd[,k],mean)
+    fdic2 = tapply(Psi[,axis2],dd[,k],mean) 
+    
+    text(fdic1,fdic2,labels=levels(dd[,k]),col=seguentColor, cex=0.6)
+    c<-c+1
+  }
+  # ordinal
+  dordi<-c(6,7)
+  dd[,dordi[2]] <- factor(dd[,dordi[2]], ordered=TRUE,  levels= c("[0-25)", "[25-50)", "[50-75)", "[75-100)", "[100-125)", "[125-150)", "[150-175)", "[175-200)",">200"))
+  c<-1
+  col <- 1
+  for(k in dordi){
+    seguentColor<-colors[col]
+    fdic1 = tapply(Psi[,axis1],dd[,k],mean)
+    fdic2 = tapply(Psi[,axis2],dd[,k],mean) 
+    
+    #points(fdic1,fdic2,pch=16,col=seguentColor, labels=levels(dd[,k]))
+    #connect modalities of qualitative variables
+    lines(fdic1,fdic2,pch=16,col=seguentColor)
+    text(fdic1,fdic2,labels=levels(dd[,k]),col=seguentColor, cex=0.6)
+    c<-c+1
+    col<-col+1
+  }
+  legend("bottomleft",names(dd)[dcat],pch=1,col=colors, cex=0.6)
+  legend("bottomright",names(dd)[dordi],pch=1,col=colors[1:length(dordi)], cex=0.6)
   
-  #points(fdic1,fdic2,pch=16,col=seguentColor, labels=levels(dd[,k]))
-  #connect modalities of qualitative variables
-  lines(fdic1,fdic2,pch=16,col=seguentColor)
- text(fdic1,fdic2,labels=levels(dd[,k]),col=seguentColor, cex=0.6)
-  c<-c+1
-  col<-col+1
 }
-legend("topleft",names(dd)[dordi],pch=1,col=colors[1:length(dordi)], cex=0.6)
 
 
+# Axis 1,2
+png(filename="ACP/PlotWithZooms1-2(1).png", width=1920, height=1080,units="px")
+plot.with.zooms(-4,4,-4,1,1,2)
+dev.off()
+png(filename="ACP/PlotWithZooms1-2(2).png", width=1920, height=1080,units="px")
+plot.with.zooms(-1,1,-1,1,1,2)
+dev.off()
+png(filename="ACP/PlotWithZooms1-2(3).png", width=1920, height=1080,units="px")
+plot.with.zooms(-2,2,-1,1,1,2)
+dev.off()
 
-# PROJECTION OF ILLUSTRATIVE qualitative variables on individuals' map
-# PROJECCIÓ OF INDIVIDUALS DIFFERENTIATING THE Dictamen
-# (we need a numeric Dictamen to color)
+# Axis 1,3
+png(filename="ACP/PlotWithZooms1-3(1).png", width=1920, height=1080,units="px")
+plot.with.zooms(-5,4,-5,2,1,3)
+dev.off()
+png(filename="ACP/PlotWithZooms1-3(2).png", width=1920, height=1080,units="px")
+plot.with.zooms(-1,1,-1,1,1,3)
+dev.off()
 
-varcat=dd[,1]
-plot(Psi[,1],Psi[,2],col=varcat)
-axis(side=1, pos= 0, labels = F, col="darkgray")
-axis(side=3, pos= 0, labels = F, col="darkgray")
-axis(side=2, pos= 0, labels = F, col="darkgray")
-axis(side=4, pos= 0, labels = F, col="darkgray")
-legend("bottomleft",levels(varcat),pch=1,col=c(1,2), cex=0.6)
-
-
-# Overproject THE CDG OF  LEVELS OF varcat
-fdic1 = tapply(Psi[,1],varcat,mean)
-fdic2 = tapply(Psi[,2],varcat,mean) 
-
-text(fdic1,fdic2,labels=levels(varcat),col="cyan", cex=0.75)
-
-
-
+# Axis 2,3
+png(filename="ACP/PlotWithZooms2-3(1).png", width=1920, height=1080,units="px")
+plot.with.zooms(-4,3,-3,2,2,3)
+dev.off()
+png(filename="ACP/PlotWithZooms2-3(2).png", width=1920, height=1080,units="px")
+plot.with.zooms(-2,2,-2,2,2,3)
+dev.off()
+png(filename="ACP/PlotWithZooms2-3(3).png", width=1920, height=1080,units="px")
+plot.with.zooms(-1,1,-1,1,2,3)
+dev.off()

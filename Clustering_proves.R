7495d144496865ead97b54bd6e47be2eb45ad897
Dan T <daniel.tarres@gmail.com>
2018-10-14 17:52:33 +0200

    clustering

--- Clustering_proves.R
+++ Clustering_proves.R
@@ -12,56 +12,108 @@ attach(diabetic_data)
 #set a list of numerical variables
 names(diabetic_data)
 
+dcon <- data.frame (adm_type_id,disch_id,adm_source_id,time_in_hpt,n_lab_proc,n_proc,n_med,n_outp,n_emerg,n_inp,n_diag)
+dim(dcon)
 
-#hierarchical clustering
+#
+# CLUSTERING
+#
 
-#euclidean distance si totes son numeriques
-dcon<-data.frame (X,adm_type_id,disch_id,adm_source_id,time_in_hpt,n_lab_proc,n_proc,n_med,n_outp,n_emerg,n_inp,n_diag)
 
-d  <- dist(dcon[1:10,])
 
-#move to Gower mixed distance to deal 
-#simoultaneously with numerical and qualitative data
+# KMEANS RUN, BUT HOW MANY CLASSES?
 
-library(cluster)
+k1 <- kmeans(dcon,5)
+names(dcon)
+print(k1)
 
-#dissimilarity matrix
-#do not include in actives the identifier variables nor the potential response variable
+attributes(k1)
 
-actives<-c(2:16)
-dissimMatrix <- daisy(diabetic_data[,actives], metric = "gower", stand=TRUE)
+k1$size
 
-distMatrix<-dissimMatrix^2
+k1$withinss
 
-h1 <- hclust(distMatrix,method="ward.D2")  # NOTICE THE COST
+k1$centers
 
-#versions noves "ward.D" i abans de plot: par(mar=rep(2,4)) si se quejara de los margenes del plot
+# LETS COMPUTE THE DECOMPOSITION OF INERTIA
 
+Bss <- sum(rowSums(k1$centers^2)*k1$size)
+Bss
+Wss <- sum(k1$withinss)
+Wss
+Tss <- k1$totss
+Tss
+
+Bss+Wss
+
+Ib1 <- 100*Bss/(Bss+Wss)
+Ib1
+
+# LETS REPEAT THE KMEANS RUN WITH K=5
+
+k2 <- kmeans(dcon,5)
+k2$size
+
+Bss <- sum(rowSums(k2$centers^2)*k2$size)
+Bss
+Wss <- sum(k2$withinss)
+Wss
+
+Ib2 <- 100*Bss/(Bss+Wss)
+Ib2
+Ib1
+
+k2$centers
+k1$centers
+
+plot(k1$centers[,3],k1$centers[,2])
+
+table(k1$cluster, k2$cluster)
+
+# WHY WE HAVE OBTAINED DIFFERENT RESULTS?, AND WHICH RUN IS BETTER?
+
+# NOW TRY K=8
+
+k3 <- kmeans(dcon,8)
+k3$size
+
+Bss <- sum(rowSums(k3$centers^2)*k3$size)
+Wss <- sum(k3$withinss)
+
+Ib3 <- 100*Bss/(Bss+Wss)
+Ib3
+
+
+# HIERARCHICAL CLUSTERING
+
+d  <- dist(dcon[1:50,])
+h1 <- hclust(d,method="ward.D")  # NOTICE THE COST
 plot(h1)
 
-k<-4
+d  <- dist(dcon)
+h1 <- hclust(d,method="ward.D")  # NOTICE THE COST
+plot(h1)
 
-c2 <- cutree(h1,k)
-c3 <- cutree(h1,2)
+# BUT WE ONLY NEED WHERE THERE ARE THE LEAPS OF THE HEIGHT
 
-#class sizes 
-table(c2)
+# WHERE ARE THER THE LEAPS? WHERE WILL YOU CUT THE DENDREOGRAM?, HOW MANY CLASSES WILL YOU OBTAIN?
 
+nc = 3
 
-table(c3, c2)
+c1 <- cutree(h1,nc)
 
-c1<-c2
-par(xpd=TRUE)
-plot(n_lab_proc,n_diag,col=c1,main="Clustering of credit data in 3 classes")
-legend(90,6,c("class1","class2","class3","class4"),pch=1,col=c(1:k), cex = 0.5)
+c1[1:20]
 
-plot(n_diag,time_in_hpt)
-plot(n_diag,time_in_hpt,col=c1,main="Clustering of credit data in 3 classes")
-legend(0.8, 15,c("class1","class2","class3"),pch=1,col=c(1:3), cex=0.6)
+nc = 5
+
+c5 <- cutree(h1,nc)
+
+c5[1:20]
 
 
-pairs(dcon[,1:7], col=c1)
-
+table(c1)
+table(c5)
+table(c1,c5)
 
 
 cdg <- aggregate(as.data.frame(dcon),list(c1),mean)
@@ -69,5 +121,89 @@ cdg
 
 plot(cdg[,1], cdg[,7])
 
+# LETS SEE THE PARTITION VISUALLY
 
-par(ask=FALSE)
+
+plot(adm_type_id,n_med,col=c1,main="Clustering of credit data in 3 classes")
+legend("topright",c("class1","class2","class3"),pch=1,col=c(1:3))
+
+
+
+#plot(RatiFin,Estalvi)
+#plot(RatiFin,Estalvi,col=c1,main="Clustering of credit data in 3 classes")
+#legend("topright",c("class1","class2","class3"),pch=1,col=c(1:3), cex=0.6)
+
+#plot(Antiguedad.Trabajo,Estalvi,col=c1,main="Clustering of credit data in 3 classes")
+#legend("topright",c("class1","class2","class3"),pch=1,col=c(1:3), cex=0.6)
+#plot(Patrimonio, Ingresos,col=c1,main="Clustering of credit data in 3 classes")
+##legend("topright",c("class1","class2","class3"),pch=1,col=c(1:3), cex=0.6)
+#plot(Patrimonio, Antiguedad.Trabajo,col=c1,main="Clustering of credit data in 3 classes")
+#legend("topright",c("class1","class2","class3"),pch=1,col=c(1:3), cex=0.6)
+
+pairs(dcon[,1:7], col=c1)
+
+#plot(FI[,1],FI[,2],col=c1,main="Clustering of credit data in 3 classes")
+#legend("topleft",c("c1","c2","c3"),pch=1,col=c(1:3))
+
+# LETS SEE THE QUALITY OF THE HIERARCHICAL PARTITION
+
+
+
+Bss <- sum(rowSums(cdg^2)*as.numeric(table(c1)))
+
+Ib4 <- 100*Bss/Tss
+Ib4
+
+
+#move to Gower mixed distance to deal 
+#simoultaneously with numerical and qualitative data
+
+library(cluster)
+
+#dissimilarity matrix
+
+actives<-c(2:16)
+actives<-c(2:29)
+dissimMatrix <- daisy(diabetic_data[,actives], metric = "gower", stand=TRUE)
+
+distMatrix<-dissimMatrix^2
+
+h1 <- hclust(distMatrix,method="ward.D")  # NOTICE THE COST
+#versions noves "ward.D" i abans de plot: par(mar=rep(2,4)) si se quejara de los margenes del plot
+
+plot(h1)
+
+c2 <- cutree(h1,4)
+
+#class sizes 
+table(c2)
+
+#comparing with other partitions
+table(c1,c2)
+
+
+names(diabetic_data)
+#disch_id
+boxplot(diabetic_data[,9]~c2, horizontal=TRUE)
+#adm_source_id
+boxplot(diabetic_data[,10]~c2, horizontal=TRUE)
+#time_in_hpt
+boxplot(diabetic_data[,11]~c2, horizontal=TRUE)
+
+pairs(dcon[,1:7], col=c2)
+
+plot(disch_id,adm_source_id,col=c2,main="Clustering of credit data in 3 classes")
+#legend("topright",levels(c2),pch=1,col=c(1:4), cex=0.6)
+
+cdg <- aggregate(as.data.frame(dcon),list(c2),mean)
+cdg
+
+plot(n_lab_proc, n_med, col= c2)
+points(cdg[,4],cdg[,5],pch=16,col="orange")
+text(cdg[,4],cdg[,5], labels=cdg[,1], pos=2, font=2, cex=0.7, col="orange")
+
+potencials<-c(3,4,6,7,10,11)
+pairs(dcon[,potencials],col=c2)
+
+#Profiling plots
+
